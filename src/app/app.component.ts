9cf36d440f2234b30f1378c23e6459af7f1ef897
Daniel Jimenez <ing.danieljimenez521994@hotmail.com>
2021-05-21 07:05:47 -0400

    Mi primer proyecto para git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre:string = 'Capitan America';
+  nombre2:string = 'DaNiEl JimenEZ es EL Capitan America';
+
+  arreglo: number[] = [1,2,3,0,3,6,7,8,9,10,18,20,21, 22, 23, 40, 41, 42, ,];
+
+  PI: number = Math.PI;
+
+  perc:number = 0.259;
+
+  salario: number = 1234.5;
+
+  activar:boolean = true;
+  
+  valorPromesa = new Promise<string>((resolve, reject) =>{
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
+
+  fecha:Date = new Date();
+  
+  idioma:string = 'es';
+  videoURL:string = 'https://www.youtube.com/embed/yJTmiht-URU';
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion:{
+      calle: 'Primera',
+      casa: 20
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { registerLocaleData } from '@angular/common'
+import localeEs from '@angular/common/locales/es'
+import localeFr from '@angular/common/locales/fr'
+
+
+registerLocaleData(localeEs);
+registerLocaleData(localeFr);
+
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DonseguroPipe } from './pipes/donseguro.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DonseguroPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:LOCALE_ID,
+      useValue: 'es'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
